56bbc5107284a0fb0a59a86ed4d4e962aef05b28
k1LoW <k1lowxb@gmail.com>
2017-09-14 13:58:25 +0900

    Use Guzzle6.2

--- src/Notifier.php
+++ src/Notifier.php
@@ -69,8 +69,7 @@ class Notifier extends \Airbrake\Notifier
                 'x-api-key' => $this->opt['apiKey']
             ],
             'json' => $notice,
-            'http_erros' => false, // Guzzle6
-            'exceptions' => false, // Guzzle5
+            'http_erros' => false,
             'timeout'  => 5.0,
         ]);
         return $response->getStatusCode() === 201;
